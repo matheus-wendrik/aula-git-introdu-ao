c3f67483c15e96d14459ee0f0621713266c31795
matheus <matheuswendrik02@gmail.com>
2024-02-01 15:16:24 -0300

    finaizado

---
+++ teste.py
@@ -0,0 +1,10 @@
+num1 = int(input("digite um numero: "))
+num2 = int(input("digite um numero: "))
+
+
+media = (num1 + num2) / 2
+
+if media >= 7:
+    print('aprovado')
+else:
+    print('reprovado')
